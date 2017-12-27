e87623dc4ff8fd95aef2f91e3336d9aadcdee828
Sebastian.Ratz <sebastian.ratz.s@gmail.com>
2017-12-27 16:42:10 +0100

    removed some bugs. triangulation works in principle, however localization dies when newly triangulated points become the majority

--- continuous_operation/processFrame.m
+++ continuous_operation/processFrame.m
@@ -8,9 +8,8 @@ function [S, R_C_W, t_C_W] = processFrame(I,prev_I,prev_S,K)
 
 % Localization
 KLT = vision.PointTracker('MaxBidirectionalError', 1) % TODO: check out possible options 
-initialize(KLT, prev_S.P', prev_I);
+initialize(KLT, prev_S.P', prev_I); 
 [P_new,point_validity] = step(KLT,I);
-%P_new = [P_KLT(:,2) P_KLT(:,1)]
 S.X=prev_S.X(:, point_validity); % only keep the points that were tracked
 S.P=P_new(point_validity,:)';
 
@@ -24,16 +23,17 @@ if plot_tracking
         pause(0.001);
 end
 
+% localize with P3P and ransac
 [R_C_W, t_C_W, max_num_inliers_history] = ransacP3P(S.X,S.P,K);
 max_num_inliers_history(end);
 T_C_W = [R_C_W, t_C_W];
 T_C_W = T_C_W(:);
 
-%% track keypoints in C
+%% track keypoints
 % parameters 
 harris_patch_size = 9;
 harris_kappa = 0.08;
-num_keypoints = 200;
+num_keypoints = 500;
 nonmaximum_supression_radius = 8;
 descriptor_radius = 9;
 match_lambda = 5;
@@ -64,7 +64,7 @@ else
     matched_database_transforms = prev_S.T(:,matched_database_index);
     
     %% Triangulate new points
-    alpha_deg = 10;
+    alpha_deg = 2;
     n_iterations = 2000;
     pixel_tolerance = 10;
     
@@ -78,8 +78,9 @@ else
     
          % triangulated points in original coordinate frame 
          P_C1 = R_C2_C1' * P_C2 - R_C2_C1' * t_C2_C1;
+         P_C1_in_C2 = P_C2 - repmat(t_C2_C1,[1,size(P_C2,2)]);
 
-         angles_deg = acosd(dot(P_C1, P_C2) ./ (norm(P_C1,2) + norm(P_C2,2)));
+         angles_deg = acosd(dot(P_C1_in_C2, P_C2) ./ (vecnorm(P_C1_in_C2,2) .* vecnorm(P_C2,2)))
 
          triangulate_mask = bitand(angles_deg > alpha_deg, best_inlier_mask);
          T_C_W = reshape(unique_transforms(:,i), 3,4);
@@ -91,6 +92,7 @@ else
          S.P = [S.P matched_query_keypoints(:,triangulate_mask)];
     end
     
+    % store matched but not triangulated and new key points to F,C,T
     if ~isempty(T_C_W) && ~isempty(matched_database_descriptors)
         unmatched_query_keypoints = query_keypoints(:,not(matched_query_mask));
         unmatched_query_descriptors = query_descriptors(:,not(matched_query_mask));
@@ -109,9 +111,4 @@ else
     
 end
     
-
-
-
-
-
 end
