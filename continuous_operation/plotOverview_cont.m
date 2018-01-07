1606d8a8ca3e70ce8299b9a4240040c787a0b285
Sebastian.Ratz <sebastian.ratz.s@gmail.com>
2018-01-07 17:46:05 +0100

    time optimized plotting

--- continuous_operation/plotOverview_cont.m
+++ continuous_operation/plotOverview_cont.m
@@ -4,6 +4,7 @@ run('param.m');
 fig1 = figure(1); 
 fig1.Position = full_screen; 
 
+
 % full trajectory
 figure(1); subplot(2,4,6); hold on; grid on; axis equal;      
     if all(size(R_C_W) > 0) && all(size(t_C_W) > 0)
@@ -17,6 +18,7 @@ figure(1); subplot(2,4,6); hold on; grid on; axis equal;
         xlabel('x'); ylabel('z');
     end
 
+
 % keypoints statistics
 subplot(2,4,5); hold on; grid on; 
     plot([i-frame_step_size,i],[prev_S.num_tracked_keypoints,S.num_tracked_keypoints],'b.-'); 
@@ -26,34 +28,39 @@ subplot(2,4,5); hold on; grid on;
     title('keypoints statistics')
 
 % 3d landmarks and coordinate frame
+
 sp2 = subplot(2,4,[3,4,7,8]); hold on; grid on;
     if sliding_window_plots
         delete(sp2);
         sp2 = subplot(2,4,[3,4,7,8]); hold on; grid on;
-        for i = 1:min(sliding_window_plots_number,size(poses,1))
-            % landmarks
-            X = landmarks_container{i};
-            if ~isempty(X)
+        
+        X = cell2mat(landmarks_container);
+        % landmarks
+        if ~isempty(X)
                 scatter3(X(1,:), X(2,:), X(3,:), 5, 'k'); 
-            end
-            set(gcf, 'GraphicsSmoothing', 'on'); view(0,0);
-            axis equal; axis vis3d;
-            xlabel('x'); ylabel('y'); zlabel('z');
-            title('Trajectory of last 20 frames and landmarks')
-            
-            % frame
+        end
+           
+        for i = 1:min(sliding_window_plots_number,size(poses,1))
+            % camera coordinate frame
             T_W_C = reshape(poses(i,:),3,4);
             R_W_C = T_W_C(1:3,1:3); t_W_C = T_W_C(1:3,4);
             T_C_W = [R_W_C', -R_W_C'*t_W_C];
             R_C_W = T_C_W(1:3,1:3); t_C_W = T_C_W(1:3,4);
+            
             plotCoordinateFrame(R_C_W', -R_C_W'*t_C_W, 2);
-            view([0,0]);
+            
             position_cam = -R_C_W'*t_C_W; 
             axis([position_cam(1)-20, position_cam(1)+20, ...
                   position_cam(2)-10, position_cam(2)+10,...
                   position_cam(3)-20, position_cam(3)+40])
-            
         end
+        
+        view([0,0]);
+        %set(gcf, 'GraphicsSmoothing', 'on'); view(0,0);
+        axis equal; axis vis3d;
+        xlabel('x'); ylabel('y'); zlabel('z');
+        title('Trajectory of last 20 frames and landmarks')
+            
     else
         if all(size(R_C_W) > 0) && all(size(t_C_W) > 0)
             plotCoordinateFrame(R_C_W', -R_C_W'*t_C_W, 2);
